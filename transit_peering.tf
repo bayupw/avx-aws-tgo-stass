f18be5d3210f83f2e07b9ab80d24ca59cd97c3af
bayupw <bayupw@gmail.com>
2022-03-18 08:28:08 +1300

    remove sta peering

--- transit_peering.tf
+++ transit_peering.tf
@@ -2,12 +2,12 @@
 # Aviatrix Transit Gateway Transit Peering
 # ---------------------------------------------------------------------------------------------------------------------
 
-resource "aviatrix_transit_gateway_peering" "stass_sta_peering" {
+/* resource "aviatrix_transit_gateway_peering" "stass_sta_peering" {
   transit_gateway_name1           = aviatrix_transit_gateway.stass_gw.gw_name
   transit_gateway_name2           = aviatrix_transit_gateway.sta_gw.gw_name
   gateway1_excluded_network_cidrs = ["0.0.0.0/0"]
   gateway2_excluded_network_cidrs = ["0.0.0.0/0"]
-}
+} */
 
 resource "aviatrix_transit_gateway_peering" "stass_dev_peering" {
   transit_gateway_name1           = aviatrix_transit_gateway.stass_gw.gw_name
